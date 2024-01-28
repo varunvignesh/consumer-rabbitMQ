fa015340552065f37c470b238b8faf9c935a915c
Vicky <vicky@cartoonmango.com>
2024-01-28 21:08:15 +0530

    updated models, error handling and logging

--- create_tables.py
+++ create_tables.py
@@ -7,62 +7,38 @@ load_dotenv()
 # Initialize connection
 conn = sqlite3.connect(os.getenv("DATABASE_NAME"))
 
+# Note: added only relevant fields required for score computation.
+# Create table account_db
+conn.execute("""
+    CREATE TABLE IF NOT EXISTS account_db (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        account_code CHAR(256) NOT NULL UNIQUE
+    )
+    """)
+
+# Create Indexes for the account_db potential query columns
+conn.execute("""
+    CREATE INDEX account_db_account_code_idx ON account_db (account_code)
+             """)
+
 # Create table product_db
 conn.execute("""
     CREATE TABLE IF NOT EXISTS product_db (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
-        index_number INTEGER,
-        available_price CHAR(20),
+        account_id INTEGER,
+        reference_product_id CHAR(200) NOT NULL UNIQUE,
         stock CHAR(20),
-        source CHAR(20),
-        reference_product_id CHAR(200)
+        FOREIGN KEY(account_id) REFERENCES account_db(id)
     )
     """)
 
 # Create Indexes for the product_db potential query columns
-conn.execute("""
-    CREATE INDEX product_db_index_number_idx ON product_db (index_number)
-             """)
-
 conn.execute("""
     CREATE INDEX product_db_stock_idx ON product_db (stock)
              """)
 
 conn.execute("""
     CREATE INDEX product_db_reference_product_id_idx ON product_db (reference_product_id)
-             """)
-
-# Create table meta_info_db
-conn.execute("""
-    CREATE TABLE IF NOT EXISTS meta_info_db (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        product_info_id INTEGER,
-        account_code CHAR(256),
-        crawl_page_counter INTEGER,
-        postal_zip_code CHAR(10),
-        postal_zip_name CHAR(256),
-        store_code CHAR(256),
-        place_name TEXT,
-        admin_name CHAR(256),
-        bundle_versions_row_pk_hash TEXT,
-        major_version_end_time DATETIME,
-        bundle_variant_field_mapping json,
-        bundle_definition BLOB,
-        fulfilment_modes BLOB,
-        seller_name CHAR(256),
-        bundle_match_type TEXT,
-        bundle_definition_hash TEXT,
-        FOREIGN KEY(product_info_id) REFERENCES ProductInfo(id)
-    )
-    """)
-
-# Create Indexes for the meta_info_db potential query columns
-conn.execute("""
-    CREATE INDEX meta_info_db_account_code_idx ON meta_info_db (account_code)
-             """)
-
-conn.execute("""
-    CREATE INDEX meta_info_db_bundle_match_type_idx ON meta_info_db (bundle_match_type)
              """)
 
 print("Created all the tables and indexes successfully")
--- receiver.py
+++ receiver.py
@@ -1,94 +1,101 @@
-import pika, sys, os, json, sqlite3, ast
+import pika
+import sys
+import os
+import json
+import sqlite3
+import ast
+import logging
 from dotenv import load_dotenv
+
+# Set up logging
+logging.basicConfig(level=logging.INFO)
 
 # Get the .env variables
 load_dotenv()
 
 # Initialize connection
-conn = sqlite3.connect(os.getenv("DATABASE_NAME"))
+with sqlite3.connect(os.getenv("DATABASE_NAME")) as conn:
 
-# use cursor for commits
-cursor= conn.cursor()
+    # use cursor for commits
+    cursor= conn.cursor()
 
-def main():
     # rabbitMQ connection
-    connection = pika.BlockingConnection(pika.ConnectionParameters(os.getenv('RABBITMQ_HOST')))
-    channel = connection.channel()
-
-    queue_name = os.getenv('RABBITMQ_QUEUE_NAME')
-
-    # creating a queue if not exists
-    channel.queue_declare(queue=queue_name)
-
-    def callback(ch, method, properties, body):
-
-        # convert string to json object/dict
-        received_data = json.loads(body)
+    with pika.BlockingConnection(pika.ConnectionParameters(os.getenv('RABBITMQ_HOST'))) as connection:
+        channel = connection.channel()
         
-        # using ast as the json data have single quotes string, else would've used json as that's specifically designed for efficient JSON processing.
-        meta_info = ast.literal_eval(received_data.get('meta_info'))
+        queue_name = os.getenv('RABBITMQ_QUEUE_NAME')
 
-        # Note: could use bulk insert for perfomarce, if the case arises.
-        # insert values for product_db
-        cursor.execute(
-            """     
-                    INSERT INTO product_db (index_number, available_price, stock, source, reference_product_id) VALUES (?, ?, ?, ?, ?)
-            """, 
-            (
-                received_data["index"],
-                received_data["available_price"],
-                received_data["stock"],
-                received_data["source"],
-                meta_info.get("reference_product_id")
-            )
-        )
+        # declare the queue
+        channel.queue_declare(queue=queue_name)
 
-        # fetch reference key the table meta_info_db
-        product_info_id = cursor.lastrowid      
+        def callback(ch, method, properties, body):
+            try: 
+                # convert string to json object/dict
+                received_data = json.loads(body)
+                
+                # using ast as the json data have single quotes string, else would've used json as that's specifically designed for efficient JSON processing.
+                meta_info = ast.literal_eval(received_data.get('meta_info'))
 
-        # insert values for meta_info_db
-        cursor.execute("""
-                    INSERT INTO meta_info_db (product_info_id, account_code, crawl_page_counter, postal_zip_code, postal_zip_name, store_code, place_name, admin_name, bundle_versions_row_pk_hash, major_version_end_time, bundle_variant_field_mapping, bundle_definition, fulfilment_modes, seller_name, bundle_match_type, bundle_definition_hash) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-                """, 
-                (
-                    product_info_id,
-                    meta_info.get("account_code"),
-                    meta_info.get("crawl_page_counter"),
-                    meta_info.get("postal_zip_code"),
-                    meta_info.get("postal_zip_name"),
-                    meta_info.get("store_code"),
-                    meta_info.get("place_name"),
-                    meta_info.get("admin_name1"),
-                    meta_info.get("bundle_versions_row_pk_hash"),
-                    meta_info.get("major_version_end_time"),
-                    json.dumps(meta_info.get("bundle_variant_field_mapping")),
-                    json.dumps(meta_info.get("bundle_definition")),
-                    json.dumps(meta_info.get("fulfilment_modes")),
-                    meta_info.get("seller_name"),
-                    meta_info.get("bundle_match_type"),
-                    meta_info.get("bundle_definition_hash"),
-                ))
+                # Start a transaction
+                cursor.execute("BEGIN")
 
-        print('Inserted into meta_info_db id: {}'.format(cursor.lastrowid))
+                # insert if not exists values in account_db
+                cursor.execute(
+                    """     
+                            INSERT OR IGNORE INTO account_db (account_code) VALUES (?)
+                    """, 
+                    (
+                        meta_info.get("account_code"),
+                    )
+                )
+
+                # fetch account_id from account_db
+                cursor.execute("SELECT id FROM account_db WHERE account_code = ?", (meta_info.get("account_code"),))
+                
+                account_id = cursor.fetchone()
+
+                # raise exception if account_code does not exists
+                if account_id is None:
+                    raise Exception('account_code: {} not found'.format(meta_info.get("account_code")))
+                else:
+                    account_id = account_id[0]
+
+                # Note: could use bulk insert for perfomarce, if the case arises.
+                # insert values for product_db.
+                cursor.execute(
+                    """     
+                            INSERT INTO product_db (account_id, reference_product_id, stock) VALUES (?, ?, ?)
+                    """, 
+                    (
+                        account_id,
+                        meta_info.get("reference_product_id"),
+                        received_data["stock"],
+                    )
+                )
+
+                logging.info('Inserted into product_db id: {}'.format(cursor.lastrowid))
+                
+                # Commit the transaction
+                cursor.execute("COMMIT")
+            
+            except Exception as e:
+                # Rollback the transaction on exception
+                cursor.execute("ROLLBACK")
+                
+                # Log the exception
+                logging.error(f"Error: {e}")
         
-        # commit connection
-        conn.commit()
-    
-    # callback declaring
-    channel.basic_consume(queue=queue_name, auto_ack=True, on_message_callback=callback)
-    
-    print(' [{}] Started listening on the queue'.format(queue_name))
-    # start listening on the channel
-    channel.start_consuming()
-
-if __name__ == '__main__':
-    try:
-        main()
-    # if stopped manually close connection
-    except KeyboardInterrupt:
-        conn.close()
-        print('Interrupted')
+        # callback declaring
+        channel.basic_consume(queue=queue_name, auto_ack=True, on_message_callback=callback)
+        
+        logging.info(' [{}] Started listening on the queue'.format(queue_name))
+       
         try:
-            sys.exit(0)
-        except SystemExit:
-            os._exit(0)
+            # start listening on the channel
+            channel.start_consuming()
+
+        except KeyboardInterrupt:
+            logging.info('Interrupted. Closing connection...')
+            channel.stop_consuming()
+
+logging.info('Script execution completed.')
